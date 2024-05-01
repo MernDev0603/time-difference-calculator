c7c4fee1ec32b63360357877f48d80993e6bc9d3
CoolDevAvenger <liu.fung.940616@gmail.com>
2024-05-01 20:28:07 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import "react-country-state-city/dist/react-country-state-city.css";
+
+import LocationPicker from './components/LocationPicker';
+import { useState } from 'react';
+
+const moment = require('moment-timezone');
+
+function getTimeDifference(fromTimeZone, toTimeZone) {
+  const format = 'YYYY-MM-DD HH:mm:ss';
+
+  const fromTime = moment.tz(fromTimeZone).format(format);
+  const toTime = moment.tz(toTimeZone).format(format);
+  const now = moment();
+
+  // const diff = moment.tz(toTime, toTimeZone).diff(moment.tz(fromTime, fromTimeZone), 'hours');
+  const offset1 = moment.tz.zone(fromTimeZone).utcOffset(now);
+  const offset2 = moment.tz.zone(toTimeZone).utcOffset(now);
+
+  const offsetDifference = (offset1 - offset2) / 60;
+
+  return {
+    time1: fromTime,
+    time2: toTime,
+    diff: offsetDifference
+  };
+}
 
 function App() {
+
+  const [timezone1, setTimezone1] = useState("America/New_York");
+  const [timezone2, setTimezone2] = useState("America/New_York");
+
+  const { time1, time2, diff } = getTimeDifference(timezone1, timezone2);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <div className='location'>
+          <LocationPicker setTime={setTimezone1}/>
+          <LocationPicker setTime={setTimezone2}/>
+        </div>
+        <div className='result'>
+          {`Current time in ${timezone1}: ${time1}`} <br/>
+          {`Current time in ${timezone2}: ${time2}`} <br/>
+          <br/>
+          {`Time difference: ${diff} hours`}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/LocationPicker.js
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import {
+    CitySelect,
+    CountrySelect,
+    StateSelect,
+} from "react-country-state-city";
+
+function LocationPicker({setTime}) {
+
+const [countryid, setCountryid] = useState(0);
+const [stateid, setstateid] = useState(0);
+const [timezone, setTimezone] = useState("");
+
+const handleChange = async (lat, lng) => {
+    const apiKey = process.env.REACT_APP_KEY;
+    const url = `https://maps.googleapis.com/maps/api/timezone/json?location=${lat},${lng}&timestamp=${Math.floor(Date.now() / 1000)}&key=${apiKey}`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        // console.log(data.timeZoneId);
+        setTimezone(data.timeZoneId);
+        setTime(data.timeZoneId);
+    } catch (error) {
+        console.error('Error fetching timezone data:', error);
+    }
+}
+
+return (
+    <div className="location-item">
+        <h3>Country</h3>
+        <CountrySelect
+            onChange={(e) => {
+                setCountryid(e.id);
+                console.log(e);
+                handleChange(e.latitude, e.longitude);
+            }}
+            placeHolder="Select Country"
+        />
+        <h3>State</h3>
+        <StateSelect
+            countryid={countryid}
+            onChange={(e) => {
+                setstateid(e.id);
+            }}
+            placeHolder="Select State"
+        />
+        <h3>City</h3>
+        <CitySelect
+            countryid={countryid}
+            stateid={stateid}
+            onChange={(e) => {
+                handleChange(e.latitude, e.longitude);
+            }}
+            placeHolder="Select City"
+        />
+        <div className="timezone">Timezone: {timezone}</div>
+    </div>
+);
+}
+
+export default LocationPicker;
